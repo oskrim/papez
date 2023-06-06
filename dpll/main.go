409965b2487f87ba853063ebc8edcfc514840834
Oskari Mantere <osku.mantere+github@gmail.com>
2023-06-06 21:32:58 +0300

    works with neg and pos literal

--- dpll/main.go
+++ dpll/main.go
@@ -21,6 +21,8 @@ type DPLL struct {
 	variables map[uint]bool
 	// Set of assignments
 	trail []uint
+	// max_key is the maximum key in variables
+	max_key uint
 }
 
 func main() {
@@ -39,11 +41,17 @@ func (dpll *DPLL) Solve() bool {
 
 func (dpll *DPLL) RegisterVariables() {
 	dpll.variables = make(map[uint]bool)
+	dpll.max_key = 0
 	for _, clause := range dpll.clauses {
 		for _, literal := range clause.literals {
 			dpll.variables[literal] = false
+			if literal > dpll.max_key {
+				dpll.max_key = literal
+			}
 		}
 	}
+	dpll.max_key -= (dpll.max_key & 1)
+	dpll.max_key += 2
 }
 
 func (dpll *DPLL) SolveInternal() bool {
@@ -52,46 +60,49 @@ func (dpll *DPLL) SolveInternal() bool {
 	}
 
 	// Try to assign a variable to true
-	for pos_lit := uint(0); true; pos_lit += 2 {
-		// if pos_lit does not exist in variables, then we have tried all variables
-		if pos_lit_value, ok := dpll.variables[pos_lit]; !ok {
-			break
-		} else {
-			neg_lit := pos_lit + 1
-			neg_lit_value := dpll.variables[neg_lit]
+	for pos_lit := uint(0); pos_lit < dpll.max_key; pos_lit += 2 {
+		pos_lit_value := dpll.variables[pos_lit]
 
-			// if pos_lit or its negation is already assigned to true, then skip
-			if pos_lit_value || neg_lit_value {
-				continue
-			}
+		neg_lit := pos_lit + 1
+		neg_lit_value := dpll.variables[neg_lit]
 
-			// assign pos_lit to true
-			dpll.variables[pos_lit] = true
-			dpll.trail = append(dpll.trail, pos_lit)
-			debug("Assigned", pos_lit, "to true")
+		// if pos_lit or its negation is already assigned to true, then skip
+		if pos_lit_value || neg_lit_value {
+			continue
+		}
 
-			// Recurse
-			if dpll.SolveInternal() {
-				return true
-			}
+		// Assign pos_lit to true
+		dpll.Push(pos_lit)
+		debug("Assigned", pos_lit, "to true")
 
-			// Try the negation of pos_lit
-			dpll.variables[pos_lit] = false
-			dpll.variables[neg_lit] = true
-			dpll.trail = append(dpll.trail, neg_lit)
-			debug("Assigned", neg_lit, "to true")
+		// Recurse
+		if dpll.SolveInternal() {
+			return true
+		}
 
-			// Recurse
-			if dpll.SolveInternal() {
-				return true
-			}
+		// Try the negation of pos_lit
+		dpll.Push(neg_lit)
+		debug("Assigned", neg_lit, "to true")
 
-			// Neither variable can be assigned to true, backtrack
-			dpll.variables[neg_lit] = false
+		// Recurse
+		if dpll.SolveInternal() {
+			return true
 		}
 	}
 
+	dpll.Pop()
+
 	return false
+}
+
+func (dpll *DPLL) Push(variable uint) {
+	dpll.variables[variable] = true
+	dpll.trail = append(dpll.trail, variable)
+}
+
+func (dpll *DPLL) Pop() {
+	dpll.variables[dpll.trail[len(dpll.trail)-1]] = false
+	dpll.trail = dpll.trail[:len(dpll.trail)-1]
 }
 
 func (dpll *DPLL) AllClausesSatisfied() bool {
--- dpll/main_test.go
+++ dpll/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"reflect"
 )
 
 func TestNoClauses(t *testing.T) {
@@ -19,5 +20,31 @@ func TestUnitSat1(t *testing.T) {
 	dpll.clauses = append(dpll.clauses, Clause{[]uint{0}})
 	if dpll.Solve() != true {
 		t.Errorf("Expected Solve() to return true, got false")
+	}
+	if !reflect.DeepEqual(dpll.trail, []uint{0}) {
+		t.Errorf("Expected trail to be [0], got %v", dpll.trail)
+	}
+	if dpll.variables[0] != true {
+		t.Errorf("Expected variables to be {0: true}, got %v", dpll.variables)
+	}
+	if dpll.max_key != 2 {
+		t.Errorf("Expected max_key to be 2, got %d", dpll.max_key)
+	}
+}
+
+func TestUnitSat2(t *testing.T) {
+	dpll := NewDPLL()
+	dpll.clauses = append(dpll.clauses, Clause{[]uint{1}})
+	if dpll.Solve() != true {
+		t.Errorf("Expected Solve() to return true, got false")
+	}
+	if !reflect.DeepEqual(dpll.trail, []uint{1}) {
+		t.Errorf("Expected trail to be [0], got %v", dpll.trail)
+	}
+	if dpll.variables[1] != true {
+		t.Errorf("Expected variables to be {1: true}, got %v", dpll.variables)
+	}
+	if dpll.max_key != 2 {
+		t.Errorf("Expected max_key to be 2, got %d", dpll.max_key)
 	}
 }
