fa2df3a17620e673d12ebf54221c3d73a8eec14f
Oskari Mantere <osku.mantere+github@gmail.com>
2023-07-10 19:02:16 +0300

    create mutable vars

--- proof/main.rs
+++ proof/main.rs
@@ -1,45 +1,18 @@
 #[derive(Debug)]
-enum Formula {
-    Var(String),
-    Implication {
-        left: Box<Formula>,
-        right: Box<Formula>,
-    }
+enum Ty {
+    TVar(usize),
+    TArr(Box<Ty>, Box<Ty>),
 }
-
-fn split_implication(formula: Formula) -> (Vec<Formula>, Formula) {
-    match formula {
-        Formula::Var(x) => (Vec::new(), Formula::Var(x)),
-        Formula::Implication { left, right } => {
-            let (args, tgt) = split_implication(*right);
-            let mut left_args = vec![*left];
-            left_args.extend(args);
-            (left_args, tgt)
-        }
-    }
-}
-
-// fn provable(f: Formula, )
-//     fn inner(f: Formula, seen: Vec::) -> bool {
-//         match f {
-//             Formula::Var(x) => false,
-//             Formula::Implication { left, right } => {
-//                 let (args, tgt) = split_implication(*right);
-//                 let mut left_args = vec![*left];
-//                 left_args.extend(args);
-//                 (left_args, tgt)
-//             }
-//         }
-//     }
 
 fn main() {
-    let x = Formula::Var("a".to_string());
-    let y = Formula::Var("b".to_string());
-    let z = Formula::Implication { left: Box::new(x), right: Box::new(y) };
+    let mut n: usize = 0;
+    let mut fresh = || {
+        n += 1;
+        Ty::TVar(n - 1)
+    };
+
+    let x = fresh();
+    let y = fresh();
+    let z = Ty::TArr(Box::new(x), Box::new(y));
     println!("z {:?}", z);
-
-    let (args, tgt) = split_implication(z);
-    println!("args {:?} tgt {:?}", args, tgt);
-
-
 }
