073f9e5fb74ba71864a5edb0808be57100fe40a7
Oskari Mantere <osku.mantere+github@gmail.com>
2023-07-11 12:11:19 +0300

    unlink and occurs

--- proof/src/main.rs
+++ proof/src/main.rs
@@ -1,13 +1,19 @@
 use std::sync::Mutex;
 use rpds::HashTrieMap;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Ty {
-    TVar(usize),
+    TVar(Tvar),
     TArr(Box<Ty>, Box<Ty>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
+enum Tvar {
+    AVar(usize),
+    Link(Box<Ty>),
+}
+
+#[derive(Debug, PartialEq)]
 enum Term {
     Var(String),
     Abs(String, Box<Term>),
@@ -27,26 +33,42 @@ fn fresh<'a>() -> usize {
     *n - 1
 }
 
-fn infer(env: &HashTrieMap<String, usize>, term: Term) -> (Ty, Vec<Teq>) {
-    match term {
-        Term::Var(x) => (Ty::TVar(*env.get(&x).unwrap()), vec![]),
-        Term::Abs(x, t) => {
-            let ax = fresh();
-            let env2 = env.insert(x, ax);
-            let (at, et) = infer(&env2, *t);
-            (Ty::TArr(Box::new(Ty::TVar(ax)), Box::new(at)), et)
-        }
-        Term::App(t, u) => {
-            let (at, et) = infer(env, *t);
-            let (au, eu) = infer(env, *u);
-            let ax = fresh();
-            let mut eret = vec![Teq(at, Ty::TArr(Box::new(au), Box::new(Ty::TVar(ax))))];
-            eret.extend(et);
-            eret.extend(eu);
-            (Ty::TVar(ax), eret)
-        }
+fn unlink(t: &Ty) -> Ty {
+    match t {
+        Ty::TVar(Tvar::AVar(x)) => Ty::TVar(Tvar::AVar(*x)),
+        Ty::TVar(Tvar::Link(t)) => unlink(t),
+        Ty::TArr(t1, t2) => Ty::TArr(Box::new(unlink(t1)), Box::new(unlink(t2))),
     }
 }
+
+fn occurs(x: usize, t: &Ty) -> bool {
+    match t {
+        Ty::TVar(Tvar::AVar(y)) => x == *y,
+        Ty::TVar(Tvar::Link(t)) => occurs(x, t),
+        Ty::TArr(t1, t2) => occurs(x, t1) || occurs(x, t2),
+    }
+}
+
+// fn infer(env: &HashTrieMap<String, usize>, term: Term) -> (Ty, Vec<Teq>) {
+//     match term {
+//         Term::Var(x) => (Ty::TVar(*env.get(&x).unwrap()), vec![]),
+//         Term::Abs(x, t) => {
+//             let ax = fresh();
+//             let env2 = env.insert(x, ax);
+//             let (at, et) = infer(&env2, *t);
+//             (Ty::TArr(Box::new(Ty::TVar(ax)), Box::new(at)), et)
+//         }
+//         Term::App(t, u) => {
+//             let (at, et) = infer(env, *t);
+//             let (au, eu) = infer(env, *u);
+//             let ax = fresh();
+//             let mut eret = vec![Teq(at, Ty::TArr(Box::new(au), Box::new(Ty::TVar(ax))))];
+//             eret.extend(et);
+//             eret.extend(eu);
+//             (Ty::TVar(ax), eret)
+//         }
+//     }
+// }
 
 fn main() {
     // λf.λx.f x
@@ -61,7 +83,7 @@ fn main() {
         )),
     );
 
-    let (ty, eqs) = infer(&HashTrieMap::new(), term);
-    println!("ty {:?}", ty);
-    println!("eqs {:?}", eqs);
+    let unlinked = unlink(&Ty::TVar(Tvar::Link(Box::new(Ty::TVar(Tvar::AVar(0))))));
+    assert!(unlinked == Ty::TVar(Tvar::AVar(0)));
+    assert!(occurs(0, &unlinked));
 }
