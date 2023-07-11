fbcfb4b65e0192c292e0d06720bb74fe2981523e
Oskari Mantere <osku.mantere+github@gmail.com>
2023-07-11 16:29:23 +0300

    bad unify

--- proof/src/main.rs
+++ proof/src/main.rs
@@ -1,13 +1,13 @@
 use std::sync::Mutex;
 use rpds::HashTrieMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Ty {
     TVar(Tvar),
     TArr(Box<Ty>, Box<Ty>),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Tvar {
     AVar(usize),
     Link(Box<Ty>),
@@ -33,7 +33,7 @@ fn fresh<'a>() -> usize {
     *n - 1
 }
 
-fn unlink(t: &Ty) -> Ty {
+fn unlink<'a>(t: &Ty) -> Ty {
     match t {
         Ty::TVar(Tvar::AVar(x)) => Ty::TVar(Tvar::AVar(*x)),
         Ty::TVar(Tvar::Link(t)) => unlink(t),
@@ -49,28 +49,71 @@ fn occurs(x: usize, t: &Ty) -> bool {
     }
 }
 
-// fn infer(env: &HashTrieMap<String, usize>, term: Term) -> (Ty, Vec<Teq>) {
-//     match term {
-//         Term::Var(x) => (Ty::TVar(*env.get(&x).unwrap()), vec![]),
-//         Term::Abs(x, t) => {
-//             let ax = fresh();
-//             let env2 = env.insert(x, ax);
-//             let (at, et) = infer(&env2, *t);
-//             (Ty::TArr(Box::new(Ty::TVar(ax)), Box::new(at)), et)
-//         }
-//         Term::App(t, u) => {
-//             let (at, et) = infer(env, *t);
-//             let (au, eu) = infer(env, *u);
-//             let ax = fresh();
-//             let mut eret = vec![Teq(at, Ty::TArr(Box::new(au), Box::new(Ty::TVar(ax))))];
-//             eret.extend(et);
-//             eret.extend(eu);
-//             (Ty::TVar(ax), eret)
-//         }
-//     }
-// }
+fn unify(a: &'static mut Ty, b: &'static mut Ty) {
+    let ua: Ty = unlink(a);
+    match (ua, b.clone()) {
+        (Ty::TVar(v), b) => {
+            match v {
+                Tvar::Link(_t) => panic!("unification failed"),
+                Tvar::AVar(x) => {
+                    if occurs(x, &b) {
+                        panic!("unification failed")
+                    } else {
+                        *a = Ty::TVar(Tvar::Link(Box::new(b)))
+                    }
+                }
+            }
+        },
+        (ua, Ty::TVar(_)) => {
+            *b = Ty::TVar(Tvar::Link(Box::new(ua)))
+        },
+        (Ty::TArr(_, _), Ty::TArr(_, _)) => {
+            let (a1, a2) = match a {
+                Ty::TArr(a1, a2) => (&mut **a1, &mut **a2),
+                _ => panic!("unification failed"),
+            };
+            let (b1, b2) = match b {
+                Ty::TArr(b1, b2) => (&mut **b1, &mut **b2),
+                _ => panic!("unification failed"),
+            };
+            unify(&mut *a1, &mut *a2);
+            unify(&mut *b1, &mut *b2);
+        },
+    }
+}
+
+static mut tyvec: Vec<Ty> = vec![];
+
+fn infer(env: &HashTrieMap<String, usize>, term: Term) -> Ty {
+    match term {
+        Term::Var(x) => Ty::TVar(Tvar::AVar(*env.get(&x).unwrap())),
+        Term::Abs(x, t) => {
+            let ax = fresh();
+            let env2 = env.insert(x, ax);
+            let b = infer(&env2, *t);
+            Ty::TArr(Box::new(Ty::TVar(Tvar::AVar(ax))), Box::new(b))
+        }
+        Term::App(t, u) => {
+            let iu = infer(env, *u);
+            let b = fresh();
+            let ab = Ty::TArr(Box::new(iu), Box::new(Ty::TVar(Tvar::AVar(b))));
+            let it = infer(env, *t);
+            unsafe {
+                tyvec.push(it);
+                tyvec.push(ab);
+                let nn = tyvec.len();
+                unify(&mut tyvec[nn-2], &mut tyvec[nn-1]);
+            }
+            Ty::TVar(Tvar::AVar(b))
+        }
+    }
+}
 
 fn main() {
+    let unlinked = unlink(&Ty::TVar(Tvar::Link(Box::new(Ty::TVar(Tvar::AVar(0))))));
+    assert!(unlinked == Ty::TVar(Tvar::AVar(0)));
+    assert!(occurs(0, &unlinked));
+
     // λf.λx.f x
     let term = Term::Abs(
         "f".to_string(),
@@ -83,7 +126,7 @@ fn main() {
         )),
     );
 
-    let unlinked = unlink(&Ty::TVar(Tvar::Link(Box::new(Ty::TVar(Tvar::AVar(0))))));
-    assert!(unlinked == Ty::TVar(Tvar::AVar(0)));
-    assert!(occurs(0, &unlinked));
+    let env: HashTrieMap<String, usize> = HashTrieMap::new();
+    let result = infer(&env, term);
+    println!("{:?}", result);
 }
