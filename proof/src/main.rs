5cfd9c2a140cabb5d80d72f3d3856040ca591b6d
Oskari Mantere <osku.mantere+github@gmail.com>
2023-07-13 10:25:59 +0300

    naive rc/refcell impl builds

--- proof/src/main.rs
+++ proof/src/main.rs
@@ -1,19 +1,60 @@
-use std::sync::Mutex;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::collections::HashMap;
 use rpds::HashTrieMap;
 
 enum Expr {
     Var(String),
-    Abs(String, Box<Expr>),
-    App(Box<Expr>, Box<Expr>),
-    Pi(String, Box<Expr>, Box<Expr>),
+    Abs(String, Rc<RefCell<Expr>>),
+    App(Rc<RefCell<Expr>>, Rc<RefCell<Expr>>),
+    Pi(String, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>),
     Type(usize),
     Nat,
     Zero,
-    Succ(Box<Expr>),
-    Ind(Box<Expr>, Box<Expr>, Box<Expr>),
-    Id(Box<Expr>, Box<Expr>, Box<Expr>),
-    Refl(Box<Expr>),
-    J(Box<Expr>, Box<Expr>, Box<Expr>, Box<Expr>, Box<Expr>, Box<Expr>),
+    Succ(Rc<RefCell<Expr>>),
+    Ind(Rc<RefCell<Expr>>, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>),
+    Id(Rc<RefCell<Expr>>, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>),
+    Refl(Rc<RefCell<Expr>>),
+    J(Rc<RefCell<Expr>>, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>, Rc<RefCell<Expr>>),
+}
+
+enum Neutral {
+    NVar(String),
+    NApp(Rc<RefCell<Neutral>>, Value),
+    NInd(Rc<RefCell<Neutral>>, Value, Value, Value),
+    NJ(Value, Value, Value, Value, Value, Rc<RefCell<Neutral>>),
+}
+
+enum Value {
+    VAbs(Rc<dyn Fn(Value) -> Value>),
+    VPi(Rc<RefCell<Value>>, Rc<dyn Fn(Value) -> Value>),
+    VType(usize),
+    VNat,
+    VZero,
+    VSucc(Rc<RefCell<Value>>),
+    VId(Rc<RefCell<Value>>, Rc<RefCell<Value>>, Rc<RefCell<Value>>),
+    VRefl(Rc<RefCell<Value>>),
+    VNeutral(Rc<RefCell<Neutral>>),
+}
+
+fn vapp(u: Value, v: Value) -> Value {
+    match u {
+        Value::VAbs(f) => f(v),
+        Value::VNeutral(n) => Value::VNeutral(Rc::new(RefCell::new(Neutral::NApp(n, v)))),
+        _ => panic!("vapp"),
+    }
+}
+
+fn eval(env: &'static mut HashMap<String, Rc<RefCell<Value>>>, t: Rc<RefCell<Expr>>) -> Rc<RefCell<Value>> {
+    match &*t.borrow() {
+        Expr::Var(x) => {
+            match env.get(x) {
+                Some(v) => Rc::clone(v),
+                None => Rc::new(RefCell::new(Value::VNeutral(Rc::new(RefCell::new(Neutral::NVar(x.to_string()))))))
+            }
+        },
+        _ => panic!("eval"),
+    }
 }
 
 fn main() {
