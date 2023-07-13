94a800e0bc0d0aae21f35e4d11b91db6d6224601
Oskari Mantere <osku.mantere+github@gmail.com>
2023-07-13 12:16:37 +0300

    implemented VAbs without fn

--- proof/src/main.rs
+++ proof/src/main.rs
@@ -1,6 +1,5 @@
-use std::rc::Rc;
-use std::collections::HashMap;
 use rpds::HashTrieMap;
+use std::rc::Rc;
 
 enum Expr {
     Var(String),
@@ -19,14 +18,14 @@ enum Expr {
 
 enum Neutral {
     NVar(String),
-    NApp(Rc<Neutral>, Value),
+    NApp(Rc<Neutral>, Rc<Value>),
     NInd(Rc<Neutral>, Value, Value, Value),
     NJ(Value, Value, Value, Value, Value, Rc<Neutral>),
 }
 
 enum Value {
-    VAbs(Rc<dyn Fn(Value) -> Value>),
-    VPi(Rc<Value>, Rc<dyn Fn(Value) -> Value>),
+    VAbs(HashTrieMap<String, Rc<Value>>, String, Rc<Expr>),
+    VPi(Rc<Value>, Rc<dyn FnOnce(Value) -> Value>),
     VType(usize),
     VNat,
     VZero,
@@ -36,22 +35,24 @@ enum Value {
     VNeutral(Rc<Neutral>),
 }
 
-fn vapp(u: Value, v: Value) -> Value {
-    match u {
-        Value::VAbs(f) => f(v),
-        Value::VNeutral(n) => Value::VNeutral(Rc::new(Neutral::NApp(n, v))),
+fn vapp(u: Rc<Value>, v: Rc<Value>) -> Rc<Value> {
+    match u.as_ref() {
+        Value::VAbs(env, x, e) => {
+            let env2 = env.insert(x.to_string(), Rc::clone(&v));
+            eval(env2, Rc::clone(&e))
+        }
+        Value::VNeutral(n) => Rc::new(Value::VNeutral(Rc::new(Neutral::NApp(Rc::clone(n), v)))),
         _ => panic!("vapp"),
     }
 }
 
-fn eval(env: &HashMap<String, Rc<Value>>, t: Rc<Expr>) -> Rc<Value> {
+fn eval(env: HashTrieMap<String, Rc<Value>>, t: Rc<Expr>) -> Rc<Value> {
     match t.as_ref() {
-        Expr::Var(x) => {
-            match env.get(x) {
-                Some(v) => Rc::clone(v),
-                None => Rc::new(Value::VNeutral(Rc::new(Neutral::NVar(x.to_string()))))
-            }
+        Expr::Var(x) => match env.get(x) {
+            Some(v) => Rc::clone(v),
+            None => Rc::new(Value::VNeutral(Rc::new(Neutral::NVar(x.to_string())))),
         },
+        Expr::Abs(x, e) => Rc::new(Value::VAbs(env.clone(), x.to_string(), e.clone())),
         _ => panic!("eval"),
     }
 }
@@ -62,10 +63,14 @@ mod tests {
 
     #[test]
     fn test_vapp() {
-        let f = Value::VAbs(Rc::new(|x| x));
+        let f = Value::VAbs(
+            HashTrieMap::new(),
+            "x".to_string(),
+            Rc::new(Expr::Var("x".to_string())),
+        );
         let v = Value::VNat;
-        let result = vapp(f, v);
-        match result {
+        let result = vapp(Rc::new(f), Rc::new(v));
+        match result.as_ref() {
             Value::VNat => (),
             _ => panic!("test_vapp"),
         }
@@ -73,18 +78,17 @@ mod tests {
 
     #[test]
     fn test_eval() {
-        let mut env = HashMap::new();
-        env.insert("x".to_string(), Rc::new(Value::VNat));
+        let env = HashTrieMap::new().insert("x".to_string(), Rc::new(Value::VNat));
 
         let x = Rc::new(Expr::Var("x".to_string()));
-        let result = eval(&env, x);
+        let result = eval(env.clone(), x);
         match result.as_ref() {
             Value::VNat => (),
             _ => panic!("test_eval x"),
         }
 
         let y = Rc::new(Expr::Var("y".to_string()));
-        let result = eval(&env, y);
+        let result = eval(env.clone(), y);
         match result.as_ref() {
             Value::VNeutral(_) => (),
             _ => panic!("test_eval y"),
